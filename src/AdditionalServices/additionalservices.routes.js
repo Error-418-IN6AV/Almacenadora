698b642b77c43809b3527fe4778376dfdbb44030
Angel <avillanueva-2021436@kinal.edu.gt>
2023-04-29 21:47:34 -0600

    Servicios Routes | Add, Get, Tes, Update

--- src/AdditionalServices/additionalservices.routes.js
+++ src/AdditionalServices/additionalservices.routes.js
@@ -3,11 +3,11 @@
 const express = require('express');
 const api = express.Router();
 const additionalservicesController = require('./additionalservices.controller');
-const { ensureAuth, isAdmin } = require();
+const { ensureAuth, isAdmin } = require('../services/authenticated');
 
 api.get('/test', additionalservicesController.test)
 api.get('/get', additionalservicesController.getServices)
-api.post('/add', additionalservicesController.add)
-api.put('/update/:id', additionalservicesController.update)
+api.post('/add', [ ensureAuth, isAdmin ], additionalservicesController.add)
+api.put('/update/:id', [ ensureAuth, isAdmin ], additionalservicesController.update)
 
 module.exports = api;
